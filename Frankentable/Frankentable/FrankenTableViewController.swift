5db600a4da4da24932e947d626752e498601c5b2
Ashlee Krammer <ashlee.alyse@me.com>
2019-02-22 22:17:12 -0500

    Display Data in Alphabetical Order

--- Frankentable/Frankentable/FrankenTableViewController.swift
+++ Frankentable/Frankentable/FrankenTableViewController.swift
@@ -10,109 +10,55 @@ import UIKit
 
 class FrankenTableViewController: UITableViewController {
 
-    var wordCountDict: [String: Int] = [:]
-    
+    var wordFrequencyDictionary: [String: Int] = [:]
+    var wordFrequencyArray = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         if let url = Bundle.main.url(forResource: "Data", withExtension: "txt"),
             let data = try? Data(contentsOf: url),
             let text = String(data: data, encoding: .utf8) {
-            
-            // here's your text
-//            print(text)
-            countWords(data: (text), dict: wordCountDict)
-//            print(wordCountDict)
+            countWords(data: (text))
+            makeDictIntoArray(dict: wordFrequencyDictionary)
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    func countWords(data: String, dict: Dictionary<String,Int>) {
-        let array = data.components(separatedBy: [" ", "\n", ".", ",", "'", "-" ])
-        var frequencyDict = dict
-        
-        for word in array {
-            if let oldFreq = frequencyDict[word] {
-                frequencyDict[word] = oldFreq + 1
+    func countWords(data: String) {
+        let dataArray = data.components(separatedBy: [" ", "\n", ".", ",", "'", "-", ])
+        var frequencyDictionary = [String:Int]()
+        for word in dataArray {
+            if let oldFrequencyDictionary = frequencyDictionary[word] {
+                frequencyDictionary[word] = oldFrequencyDictionary + 1
             } else {
-               frequencyDict[word] = 1
+               frequencyDictionary[word] = 1
             }
-//            wordCountDict = frequencyDict
         }
-        wordCountDict = frequencyDict
-
+        wordFrequencyDictionary = frequencyDictionary
     }
-
-    // MARK: - Table view data source
-
+    
+    func makeDictIntoArray(dict: [String:Int]) {
+        var arr = [String]()
+        for (k,v) in dict {
+            let temp = "\(k.capitalized), (\(v))"
+            arr.append(temp)
+        }
+        wordFrequencyArray = arr.sorted()
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return wordCountDict.count
+        return wordFrequencyArray.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        for (k,v) in wordCountDict {
-            print(k,v)
-//            cell.textLabel?.text = "\(k, v)"
-        }
+        let rowToSetUp = indexPath.row
+        let dataToDisplay = wordFrequencyArray[rowToSetUp]
+        cell.textLabel?.text = dataToDisplay
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
